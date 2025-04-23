124917e0ed8e9449bef1c123bfaa0b7bb6c66be6
= <muhammadalishuhratjonov1122@gmail.com>
2025-04-23 19:02:00 +0500

    added new 3

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! XN_2"', () => {
+      expect(appController.getHello()).toBe('Hello World! XN_2');
     });
   });
 });
